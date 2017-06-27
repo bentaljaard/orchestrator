bf59659580cf7f302dbb3d4a44dbbbf857dd20df
phyberphantom <phyberphantom@gmail.com>
2017-06-27 13:22:05 -0400

    updated

--- orchestrator.py
+++ orchestrator.py
@@ -3,13 +3,19 @@
 import yaml
 import queue
 from pprint import pprint
+from redis import Redis
+from rq import Queue
+from job import printTaskName
+
+	
 
 file = "test.yaml"
 with open(file) as data_file:    
 	    orchdata = yaml.load(data_file)
 
 
-q = queue.Queue()
+# q = queue.Queue()
+q = Queue(connection=Redis())
 
 tasks = orchdata['tasks']
 
@@ -18,18 +24,21 @@ for task in tasks:
 	value=task.get(key)
 
 	if(value.get('type') == 'service'):
-		q.put(value)
-q.put(None)
+		jobtask = q.enqueue(printTaskName,task)
+# 		print(jobtask.result)
+		# enqueue task
+# 		q.put(value)
+# q.put(None)
 
 # for task in tasks:
 # 	next(iter(task))
 # 	pprint(task.get().get('type'))
 
-hasMoreItems = True
+# hasMoreItems = True
 
-while hasMoreItems:
-	taskItem = q.get()
-	if taskItem is None:
-		hasMoreItems = False
-	print(taskItem)
-	q.task_done()
+# while hasMoreItems:
+# 	taskItem = q.get()
+# 	if taskItem is None:
+# 		hasMoreItems = False
+# 	print(taskItem)
+# 	q.task_done()
